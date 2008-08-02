a26563ff5cbccf8c3bc74282da5f7543d264d292
Lars Klevan <tastybyte@gmail.com>
2008-08-02 16:52:11 -0500

    add install hook

--- install.rb
+++ install.rb
@@ -1,1 +1,10 @@
-# Install hook code here
+require 'erb'
+
+config = File.dirname(__FILE__) + '/../../../config/yui_editor.yml'
+
+unless File.exist? initializer
+  initializer_template = IO.read(File.dirname(__FILE__) + '/yui_editor.yml.tpl')
+  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
+end
+
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
