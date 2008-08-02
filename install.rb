dc2446136baea1b76a8ae47b0fb4482859904bb7
Lars Klevan <tastybyte@gmail.com>
2008-08-02 16:53:07 -0500

    fix install.rb

--- install.rb
+++ install.rb
@@ -2,9 +2,9 @@ require 'erb'
 
 config = File.dirname(__FILE__) + '/../../../config/yui_editor.yml'
 
-unless File.exist? initializer
-  initializer_template = IO.read(File.dirname(__FILE__) + '/yui_editor.yml.tpl')
-  File.open(initializer, 'w') { |f| f << ERB.new(initializer_template).result }
+unless File.exist? config
+  config_template = IO.read(File.dirname(__FILE__) + '/yui_editor.yml.tpl')
+  File.open(config, 'w') { |f| f << ERB.new(config_template).result }
 end
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
